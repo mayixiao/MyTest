4baec552e563f543e961b304eeb7b0161bf9caca
mayixiao <342249838@qq.com>
2017-03-17 23:15:23 +0800

    测试

---
+++ MyTest/src/com/xy/Test.java
@@ -0,0 +1,12 @@
+package com.xy;
+
+/**
+ * Created by sml on 2017/3/17.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWord");
+    }
+
+}
